41b81fa4e686544878fa5bf49a6ca76e16b9c175
Jamel Yassin <jamelyassin84@gmail.com>
2021-12-03 22:42:17 +0400

    TopBar

--- components/ScrollVIewWithRefresh.tsx
+++ components/ScrollVIewWithRefresh.tsx
@@ -1,14 +1,34 @@
 import React, { FC } from 'react'
-import { Text, View } from 'react-native'
+import { Dimensions, RefreshControl, Text, View } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
+import Colors from '../constants/Colors'
+import useColorScheme from '../hooks/useColorScheme'
 
-type Props = {}
-
-const ScrollVIewWithRefresh: FC<Props> = (props) => {
-	return (
-		<View>
-			<Text>ScrollVIewWithRefresh</Text>
-		</View>
-	)
+type Props = {
+	loading: boolean
+	onRefresh: Function
 }
 
+const ScrollVIewWithRefresh: FC<Props> = (props) => {
+	const colorScheme = useColorScheme()
+
+	return (
+		<ScrollView
+			style={{
+				height: Dimensions.get('screen').height,
+				width: Dimensions.get('screen').width,
+			}}
+			showsVerticalScrollIndicator={false}
+			refreshControl={
+				<RefreshControl
+					tintColor={Colors[colorScheme].tint}
+					refreshing={props.loading}
+					onRefresh={props.onRefresh()}
+				/>
+			}>
+			{props.children}
+			<View style={{ height: 180 }}></View>
+		</ScrollView>
+	)
+}
 export default ScrollVIewWithRefresh
--- modules/home/home_page/_Home.tsx
+++ modules/home/home_page/_Home.tsx
@@ -1,13 +1,19 @@
 import React, { FC } from 'react'
 import { Text, View } from 'react-native'
 import AppScreen from '../../../components/Screen'
+import ScrollVIewWithRefresh from '../../../components/ScrollVIewWithRefresh'
+import HomeTopBar from './HomeTopBar'
 
 type Props = {}
 
 const _Home: FC<Props> = (props) => {
 	return (
 		<AppScreen>
-			<Text>_Home</Text>
+			<HomeTopBar />
+			<ScrollVIewWithRefresh onRefresh={() => {}} loading={false}>
+				<View style={{ marginTop: 22 }}></View>
+				<Text>_Home</Text>
+			</ScrollVIewWithRefresh>
 		</AppScreen>
 	)
 }
